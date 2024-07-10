3260f8d26d861968660de5301a87f0edb86143ba
Jhon G. Botello <jbote001@odu.edu>
2024-07-10 13:14:59 -0400

    Update get_timemaps.py

--- HW3-Web Archiving/get Timemaps/get_timemaps.py
+++ HW3-Web Archiving/get Timemaps/get_timemaps.py
@@ -3,27 +3,25 @@ import os
 import hashlib
 import time
 
-# Nombre del archivo que contiene las URIs
+# Name of the file containing the URIs
 input_file = "collected_uris.txt"
 
-# Carpeta donde se guardarán los TimeMaps
+# Folder where I will save the TimeMaps
 output_folder = "timemaps"
 
-# Crear la carpeta de salida si no existe
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# Comando base de Memgator
+# Execute memgator
 memgator_cmd = "memgator-windows-amd64 -a https://raw.githubusercontent.com/odu-cs432-websci/public/main/archives.json -F 2 -f JSON"
 
-# Leer el archivo de URIs y recuperar los TimeMaps
 with open(input_file, "r") as f:
     for uri in f:
-        uri = uri.strip()  # Eliminar espacios en blanco
-        md5_hash = hashlib.md5(uri.encode()).hexdigest()  # Calcular hash MD5
-        output_file = os.path.join(output_folder, f"timemap_{md5_hash}.json")  # Nombre del archivo de salida
-        subprocess.run(f"{memgator_cmd} {uri} > {output_file}", shell=True)  # Ejecutar el comando
+        uri = uri.strip()  # Eliminate blank spaces
+        md5_hash = hashlib.md5(uri.encode()).hexdigest()  # for hash
+        output_file = os.path.join(output_folder, f"timemap_{md5_hash}.json")  # Name for the file
+        subprocess.run(f"{memgator_cmd} {uri} > {output_file}", shell=True)
         print(f"TimeMap retrieved for URI {uri} with hash {md5_hash}")
-        time.sleep(15)  # Esperar 15 segundos entre llamadas
+        time.sleep(15)  # Add time sleep
 
 print("All TimeMaps retrieved and saved in the 'timemaps' folder.")
